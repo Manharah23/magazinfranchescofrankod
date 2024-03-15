4b2d51432db2e611f949cdb9abe1e11f5a6da4cf
Francisco Magure <magure9323@gmail.com>
2024-03-15 02:11:25 +0200

    part4

--- activacao.js
+++ activacao.js
@@ -10,3 +10,20 @@ function menucelular(){
         menuitens.style.maxHeight = "0px";
     }
 }
+
+var produtoimg = document.getElementById("produtoimg");
+
+var miniatura = document.getElementsByClassName("miniatura");
+
+miniatura[0].onclick = function(){
+    produtoimg.src = miniatura[0].src;
+}
+miniatura[1].onclick = function(){
+    produtoimg.src = miniatura[1].src;
+}
+miniatura[2].onclick = function(){
+    produtoimg.src = miniatura[2].src;
+}
+miniatura[3].onclick = function(){
+    produtoimg.src = miniatura[3].src;
+}
